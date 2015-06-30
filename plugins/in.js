d27273f9a7301722884315ecfa6a69f41646d38a
Julius Härtl <github@jus.li>
2015-06-30 20:55:10 +0200

    Set open state in topic

--- plugins/in.js
+++ plugins/in.js
@@ -4,21 +4,48 @@ var helpers = require("../helpers");
 var request = require('request');
 
 var bot = null;
+var api;
+var open = false;
+var open_old = false;
+var initial_set= true;
 
-var inHandler = function(nick, to, text, message) {
-
+var getApi = function() {
 	var in_url = "http://hades.bitgrid.net/~jus/in/api.php?action=current";
 	request({
 		url: in_url,
 		json: true
 	}, function(error, response, body) {
 		if (!error && response.statusCode === 200) {
-			if(body['count'] == 0)
-				bot.respond(message, "Nobody here!");
+			api = body
+				open_old = open;
+			if(api['count']>0)
+				open = true;
 			else
-				bot.respond(message, body['users'].join(', '));
+				open = false;
+			setOpenState();
 		}
 	});
+}
+
+var setOpenState = function() {
+	if(open == open_old && !initial_set)
+		return;
+	if(open) {
+		bot.setTopic(" http://hackzogtum-coburg.de/  | Space OPEN | Anwesenheitsliste: http://in.hackzogtum-coburg.de/ ");
+	} else {
+		bot.setTopic(" http://hackzogtum-coburg.de/  | Space CLOSED | Anwesenheitsliste: http://in.hackzogtum-coburg.de/ ");
+	}
+	initial_set = false;
+}
+
+var inHandler = function(nick, to, text, message) {
+
+	getApi();
+
+	if(api['count'] == 0)
+		bot.respond(message, "Nobody here!");
+	else
+		bot.respond(message, api['users'].join(', '));
 
 };
 
@@ -26,6 +53,10 @@ module.exports = {
 	register: function(b) {
 
 		bot = b;
+
+		setInterval(function() {
+			getApi();
+		}, 60*1000);
 
 		bot.addMessageAction(function(nick, to, text, message){
 			if(text.indexOf("!in") == -1)
