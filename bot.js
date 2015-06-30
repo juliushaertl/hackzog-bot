fcd7f847c124abcdbf3a0c1bd1afe885f9e71a30
Julius Härtl <github@jus.li>
2015-06-30 20:44:26 +0200

    Fix login issues and add function to set topic

--- bot.js
+++ bot.js
@@ -14,21 +14,21 @@ module.exports = {
 
 		bot = new irc.Client(this.config.server, this.config.user, {
 			channels: [this.config.channel],
-			autoConnect: false,
+			showErrors: true,
+			autoConnect: true,
+			autoRejoin: true,
+			password: module.exports.config.user_password,
 		});
-
-		bot.connect(3, function() {
-			bot.say("nickserv", "identify "+ module.exports.config.password);
-		});
-
 		console.log("[hackzog-bot] connecting to server");
 		bot.addListener("registered", function(message) {
 			console.log("[hackzog-bot] connection established");
+			bot.say('nickserv', 'IDENTIFY ' + module.exports.config.user_password);
 		});
 		bot.addListener("names", function(channel, nicks) {
 			users = nicks;
 		});
 		bot.addListener("message", function(nick, to, text, message) {
+			console.log("[hackzog-bot] message nick + " - " + to + ": " + text);
 			if(text=="!help") {
 				module.exports.respond(message, "!in         show active users");
 				module.exports.respond(message, "!top        show top scores");
@@ -48,7 +48,7 @@ module.exports = {
 
 	names: function() {
 		return users;
-	},	
+	},
 
 	// Load plugins from array list
 	loadPlugins: function(plugins) {
@@ -62,22 +62,27 @@ module.exports = {
 	},
 
 	addMessageAction: function(match, callback) {
-		plugins.push([match, callback]);	
+		plugins.push([match, callback]);
 	},
 
 	addListener: function(type, callback) {
 		bot.addListener(type, callback);
 	},
-	
-	/* Let the bot say something in the public channel */
+
+		/* Let the bot say something in the public channel */
 	say: function(message) {
 		bot.say(this.config.channel, message);
 	},
 
-	/* Let the bot respond to a message public or private */
+		/* Let the bot respond to a message public or private */
 	respond: function(message, response) {
 		var returnpath = helpers.findReturnPath(message);
 		bot.say(returnpath, response);
+	},
+
+	setTopic: function(text) {
+		console.log("[hackzog-bot] set topic: " + text);
+		bot.say("ChanServ", "TOPIC "+ this.config.channel + " " + text);
 	},
 
 };
