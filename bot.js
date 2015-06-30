eba2a63a3ed4a7ad0693670485c559c72be7b303
Julius Härtl <github@jus.li>
2015-06-30 22:46:55 +0200

    Make plugins accessible via bot object

--- bot.js
+++ bot.js
@@ -2,7 +2,9 @@ var irc = require("irc");
 var helpers = require("./helpers");
 var bot;
 var users = [];
-var plugins = [];
+var plugin_actions = [];
+var plugins = {};
+var active = false;
 
 module.exports = {
 
@@ -23,22 +25,24 @@ module.exports = {
 		bot.addListener("registered", function(message) {
 			console.log("[hackzog-bot] connection established");
 			bot.say('nickserv', 'IDENTIFY ' + module.exports.config.user_password);
+			active = true;
 		});
 		bot.addListener("names", function(channel, nicks) {
 			users = nicks;
 		});
 		bot.addListener("message", function(nick, to, text, message) {
-			console.log("[hackzog-bot] message nick + " - " + to + ": " + text);
+			console.log("[hackzog-bot] message " +nick + " - " + to + ": " + text);
 			if(text=="!help") {
+				module.exports.respond(message, "!countdown  show days until events");
 				module.exports.respond(message, "!in         show active users");
 				module.exports.respond(message, "!top        show top scores");
 				module.exports.respond(message, "!karma NAME show karma of a user");
 				module.exports.respond(message, "NAME +1     increment score for user");
 				return;
 			}
-			for(var i=0; i<plugins.length; i++) {
-				if(plugins[i][0](nick, to, text, message)) {
-					plugins[i][1](nick, to, text, message);
+			for(var i=0; i<plugin_actions.length; i++) {
+				if(plugin_actions[i][0](nick, to, text, message)) {
+					plugin_actions[i][1](nick, to, text, message);
 					return;
 				}
 			}
@@ -50,19 +54,20 @@ module.exports = {
 		return users;
 	},
 
-	// Load plugins from array list
-	loadPlugins: function(plugins) {
+		// Load plugins from array list
+	loadPlugins: function(plugins_load) {
 
-		for(var i = 0, len = plugins.length; i < len; i++) {
-			var name = plugins[i];
-			var plugin = require('./plugins/'+name);
-			plugin.register(this);
+		for(var i = 0, len = plugins_load.length; i < len; i++) {
+			var name = plugins_load[i];
+			plugins[name] = require('./plugins/'+name);
+			plugins[name].register(this);
+			console.log("[hackzog-bot] load plugin " + name);
 		}
 
 	},
 
 	addMessageAction: function(match, callback) {
-		plugins.push([match, callback]);
+		plugin_actions.push([match, callback]);
 	},
 
 	addListener: function(type, callback) {
@@ -82,7 +87,13 @@ module.exports = {
 
 	setTopic: function(text) {
 		console.log("[hackzog-bot] set topic: " + text);
-		bot.say("ChanServ", "TOPIC "+ this.config.channel + " " + text);
+		if(!active)
+			setTimeout(function() {
+				module.exports.setTopic(text);
+			}, 5000);
+		else
+			bot.say("ChanServ", "TOPIC "+ this.config.channel + " " + text);
 	},
+	plugins: plugins,
 
 };
